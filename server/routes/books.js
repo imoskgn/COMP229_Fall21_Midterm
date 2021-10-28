f00959e9d1a39fd78e0b9b0143661701ba49918b
Oscar Gonzalez <imoskgn@gmail.com>
2021-10-28 01:37:30 -0400

    Fizing typo

--- server/routes/books.js
+++ server/routes/books.js
@@ -4,12 +4,12 @@ let router = express.Router();
 let mongoose = require("mongoose");
 
 // define the book model
-let Book = require("../models/books");
+let book = require("../models/books");
 
 /* GET books List page. READ */
 router.get("/", (req, res, next) => {
   // find all books in the books collection
-  Book.find((err, books) => {
+  book.find((err, books) => {
     if (err) {
       return console.error(err);
     } else {
