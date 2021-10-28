f8aaa178ccaacc58c5f6715badd25c4b6a48d1cf
Oscar Gonzalez <imoskgn@gmail.com>
2021-10-28 00:35:19 -0400

    Fixing db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://imoskgn:bIK6RzMD3YRFYCQF@cluster0.bxctd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://imoskgn:bIK6RzMD3YRFYCQF@cluster0.bxctd.mongodb.net/book_list?retryWrites=true&w=majority"
 };
--- server/routes/books.js
+++ server/routes/books.js
@@ -4,12 +4,12 @@ let router = express.Router();
 let mongoose = require("mongoose");
 
 // define the book model
-let book = require("../models/books");
+let Book = require("../models/books");
 
 /* GET books List page. READ */
 router.get("/", (req, res, next) => {
   // find all books in the books collection
-  book.find((err, books) => {
+  Book.find((err, books) => {
     if (err) {
       return console.error(err);
     } else {
